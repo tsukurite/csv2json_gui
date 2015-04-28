1f581ae09c20b7f2493068582ebe2eff7eadac22
jun <sv.junic@gmail.com>
2015-04-28 18:48:44 +0900

    コンパイル時にnwがあるとwindowで実行ファイルを作るときに躓くようなので入らないように変更

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+cp ./prod_package.json csv2json/package.json
+
 cd ./csv2json
 
+mv ./node_modules ../.node_modules
+
+npm install
+
 zip app.nw -r ./*
+
+rm -rf ./node_modules
+mv ../.node_modules node_modules
 
 cd ../
 
 cp csv2json/app.nw build/Windows/32bit/_resource/
 cp csv2json/app.nw build/Windows/64bit/_resource/
 mv csv2json/app.nw build/MacOS/csv2json.app/Contents/Resources/app.nw
+
+cp ./dev_package.json csv2json/package.json
