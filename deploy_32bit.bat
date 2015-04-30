3a5cebaf9ab59f9052493a146acd6a3866ea4e19
jun <sv.junic@gmail.com>
2015-04-30 10:21:37 +0900

    bat修正

--- deploy_32bit.bat
+++ deploy_32bit.bat
@@ -1,3 +1,2 @@
 cd ./build/Windows/32bit/_resource/
 copy /b nw.exe+app.nw csv2json.exe
-move csv2json.exe ../
--- deploy_64bit.bat
+++ deploy_64bit.bat
@@ -1,3 +1,2 @@
-# cd ./build/Windows/64bit/_resource/
+# cd ./build/Windows/64bit/
 # copy /b nw.exe+app.nw csv2json.exe
-# move csv2json.exe ../
